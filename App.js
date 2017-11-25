9651017ead77257f27251668053da1100d207567
Rajan Singh <rajan@lawscout.ca>
2017-11-24 21:21:44 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react'
+import {
+  View
+} from 'react-native'
+
+import Header from './src/components/Header.js'
+import AlbumList from './src/components//AlbumList'
+
+export default class App extends Component<{}> {
+  render () {
+    return (
+      <View style={{ flex: 1 }}>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+      </View>
+    )
+  }
+}
